fc01139623bd6ae98da3ad24fb1af4a7586bf411
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-09-17 00:41:25 +0530

    added react logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Calculator from "./components/Calculator/Calculator";
 
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <Calculator />
     </div>
   );
